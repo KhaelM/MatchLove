7174ba006c3b0d5260323ac6f351760e83b72359
KhaelM <mikerandria@gmail.com>
2018-06-28 20:16:07 +0300

    Signup form fini

--- WEB/js/signup.js
+++ WEB/js/signup.js
@@ -1,51 +1,30 @@
-$(document).ready(function () {
-    var navListItems = $('div.setup-panel-3 div a'),
-        allWells = $('.setup-content-3'),
-        allNextBtn = $('.nextBtn-3'),
-        allPrevBtn = $('.prevBtn-3');
+var slideActuel = 0;
+montrerSlide(slideActuel);
 
-    allWells.hide();
+function montrerSlide(indiceDuSlide) {
+    var listeTab = document.getElementsByClassName("tab");
+    listeTab[indiceDuSlide].style.display = "inline-block";
+    fixerEtape(indiceDuSlide);
+}
 
-    navListItems.click(function (e) {
-        e.preventDefault();
-        var $target = $($(this).attr('href')),
-            $item = $(this);
+function suivantPrecedent(n) {
+    var listeTab = document.getElementsByClassName("tab");
 
-        if (!$item.hasClass('disabled')) {
-            navListItems.removeClass('btn-info').addClass('btn-pink');
-            $item.addClass('btn-info');
-            allWells.hide();
-            $target.show();
-            $target.find('input:eq(0)').focus();
-        }
-    });
+    if( (slideActuel == 0 && n == -1) || (slideActuel == listeTab.length-1 && n == 1)) {
+        return false;
+    }
 
-    allPrevBtn.click(function(){
-        var curStep = $(this).closest(".setup-content-3"),
-            curStepBtn = curStep.attr("id"),
-            prevStepSteps = $('div.setup-panel-3 div a[href="#' + curStepBtn + '"]').parent().prev().children("a");
+    // Effacer slide précédent
+    listeTab[slideActuel].style.display = "none";
+    slideActuel += n;
+    montrerSlide(slideActuel);
 
-            prevStepSteps.removeAttr('disabled').trigger('click');
-    });
+}
 
-    allNextBtn.click(function(){
-        var curStep = $(this).closest(".setup-content-3"),
-            curStepBtn = curStep.attr("id"),
-            nextStepSteps = $('div.setup-panel-3 div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
-            curInputs = curStep.find("input[type='text'],input[type='url']"),
-            isValid = true;
-
-        $(".form-group").removeClass("has-error");
-            for(var i=0; i< curInputs.length; i++){
-                if (!curInputs[i].validity.valid){
-                    isValid = false;
-                    $(curInputs[i]).closest(".form-group").addClass("has-error");
-                }
-            }
-
-            if (isValid)
-                nextStepSteps.removeAttr('disabled').trigger('click');
-    });
-
-    $('div.setup-panel-3 div a.btn-info').trigger('click');
-});
+function fixerEtape(indiceDuSlide) {
+    var i, listeEtape = document.getElementsByClassName("etape");
+    for(i = 0; i < listeEtape.length; i++) {
+        listeEtape[i].className = listeEtape[i].className.replace(" actif", "");
+    }
+    listeEtape[indiceDuSlide].className += " actif";
+}
